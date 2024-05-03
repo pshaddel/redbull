c79cf921595e8b9c24885dbef1916e9e2df98f70
Poorshad <p.shaddel@gmail.com>
2024-05-03 20:15:36 +0200

    refactor: improve email validation in user model

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -4,9 +4,12 @@ import { z } from "zod";
 const passwordRegex =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 // move this to error handler
+const emailRegex = // eslint-disable-next-line no-control-regex
+  /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/gm;
 const passwordError =
   "Password must contain at least 8 characters and a combination of lowercase, uppercase, number and a special character";
 
+const emailError = "Invalid Email";
 //
 const UserSchema = new Schema(
   {
@@ -14,11 +17,12 @@ const UserSchema = new Schema(
       type: String,
       required: true,
       unique: true,
+      index: true,
       validate: {
         validator: (value: string) => {
-          return z.string().email().safeParse(value).success;
+          return usernameValidator.safeParse(value).success;
         },
-        message: "Invalid Email"
+        message: emailError
       }
     },
     password: {
@@ -26,7 +30,7 @@ const UserSchema = new Schema(
       required: true,
       validate: {
         validator: (value: string) => {
-          return passwordRegex.test(value);
+          return passwordValidator.safeParse(value).success;
         },
         message: passwordError
       }
@@ -34,6 +38,9 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// compound index of username and password for very fast authentication
+UserSchema.index({ username: 1, password: 1 });
 
 export type User = InferSchemaType<typeof UserSchema>;
 
@@ -57,10 +64,7 @@ const passwordValidator = z
  * Check this link: https://learn.microsoft.com/en-us/archive/blogs/testing123/email-address-test-cases
  * @example test@gmail.com
  * */
-const usernameValidator = z.string().regex(
-  // eslint-disable-next-line no-control-regex
-  /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/gm
-);
+const usernameValidator = z.string().regex(emailRegex, { message: emailError });
 export const userRegistrationValidator = z.object({
   username: usernameValidator,
   password: passwordValidator
