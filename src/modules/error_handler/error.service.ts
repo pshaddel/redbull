010530f19e6883849e4c9d78282acde55c18c9fe
Poorshad <p.shaddel@gmail.com>
2024-05-05 13:27:55 +0200

    feat: :goal_net: add error class to return a unique kind of errors

--- src/modules/error_handler/error.service.ts
+++ src/modules/error_handler/error.service.ts
@@ -12,3 +12,31 @@ export function ZodErrorHandler(error: unknown) {
     throw error;
   }
 }
+
+export class StandardError {
+  public error: string;
+  public httpStatusCode: number;
+  public extraInformation?: string;
+  constructor(
+    error: ErrorMessages,
+    /**
+     * We use this for loggs and it should not be sent to the client
+     */
+    extraInformation?: string
+  ) {
+    this.error = error;
+    this.httpStatusCode = errors[error];
+    this.extraInformation = extraInformation;
+  }
+}
+
+const errors = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  EMAIL_ALREADY_EXISTS: 400,
+  USERNAME_OR_PASSWORD_INCORRECT: 401
+} as const;
+export type ErrorMessages = keyof typeof errors;
